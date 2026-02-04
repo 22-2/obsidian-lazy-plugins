72616bbf6a08d6749fc547805ee4bac1a95ed085
22-2 <17890m@protonmail.ch>
2026-02-05 05:50:04 +0900

    add fetchPlugin

--- tests/plugin.spec.ts
+++ tests/plugin.spec.ts
@@ -1,17 +1,20 @@
 import path from "node:path";
-import { test, expect } from "obsidian-e2e-toolkit";
+import { test, expect, fetchPlugin } from "obsidian-e2e-toolkit";
 
 import { fileURLToPath } from "url"
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+test.beforeAll(async () => {
+    await fetchPlugin("https://github.com/TfTHacker/obsidian42-brat.git", "myfiles/obsidian42-brat");
+});
 
 test.use({
     vaultOptions: {
         fresh: true,
         plugins: [
             {
-                path: path.resolve(__dirname, "..", "myfiles", "plugin.js"),
-                pluginId: "sample-plugin",
+                path: path.resolve(__dirname, "..", "myfiles", "obsidian42-brat"),
             },
         ],
     },
