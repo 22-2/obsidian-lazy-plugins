e6547feb99ec74d427861120734a135206dc37e6
22-2 <17890m@protonmail.ch>
2026-02-18 13:28:07 +0900

    fix eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,14 +2,18 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import prettier from "eslint-config-prettier";
+import pathPlugin from "eslint-plugin-path";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
     {
         files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-        plugins: { js },
+        plugins: { js, path: pathPlugin },
         extends: ["js/recommended"],
         languageOptions: { globals: { ...globals.browser, ...globals.node } },
+        rules: {
+            "path/no-relative-imports": "error",
+        },
     },
     tseslint.configs.recommended,
     prettier,
